c071611a4adc42539ba7e6b5f57367c7af33deb4
Ingo Oeser <ingo@jimdo.com>
2013-03-26 09:42:06 +0100

    factor out backoff selection, limit interval
    * Factored out backoff calculation logic to enable unit testing of it
    * Have upper limit (ceiling) of returned retry interval make better
      guarantees for the API user on the range of retries on less available
      reconnection targets.

--- backoff.go
+++ backoff.go
@@ -5,6 +5,22 @@ import (
 	"time"
 )
 
+// Given a retry count and a base retry interval, return the next retry interval
+// according to the exponential backoff algorithm.
+// Maximum returned retry interval is interval * 2^15.
+func nextExponentialBackoff(retries uint, interval time.Duration) time.Duration {
+	switch {
+	case retries == 0:
+		return time.Duration(rand.Int63n(int64(interval)))
+	case retries == 1:
+		return time.Duration(rand.Int63n(7)) * interval
+	case retries >= 2 && retries < 15:
+		return time.Duration(rand.Int63n((1<<retries)-1)) * interval
+	default:
+	}
+	return time.Duration(rand.Int63n(1<<15)) * interval
+}
+
 // Retry a Reconnect on r with exponential backoff algorithm, at most c times and slot time
 // set to interval. (see http://en.wikipedia.org/wiki/Exponential_backoff for details)
 func ExponentialBackoff(r Reconnectable, interval time.Duration, max uint) (err error) {
@@ -13,14 +29,7 @@ func ExponentialBackoff(r Reconnectable, interval time.Duration, max uint) (err 
 		if err, retry = reconnectOnce(r); !retry {
 			return err
 		}
-		switch retries {
-		case 0:
-			time.Sleep(time.Duration(rand.Int63n(int64(interval))))
-		case 1:
-			time.Sleep(time.Duration(rand.Int63n(7)) * interval)
-		default:
-			time.Sleep(time.Duration(rand.Int63n((1<<retries)-1)) * interval)
-		}
+		time.Sleep(nextExponentialBackoff(retries, interval))
 	}
 	return err
 }
